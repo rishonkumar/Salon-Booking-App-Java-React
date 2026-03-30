09852bba373545f5e590bcc0a910c58c86420d1f
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-03-30 09:31:29 +0530

    Added Salon DTOs and Modal

---
+++ microservices/salon-service/src/main/java/com/salon/salon_service/payload/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.salon.salon_service.payload.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String fullName;
+    private String email;
+}
